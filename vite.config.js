4d77b9591333fbfc9a726008859788bd5f4d58f1
ghobs91 <aghobrial@ptc.com>
2024-04-05 06:23:16 -0400

    added sveltekit adapter

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		exclude: ['bytemd']
+	},
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
 };
 
